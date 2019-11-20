8d09d02476744489316c52ad918cf613595b30b7
truongtv <truongtv@rabiloo.com.vn>
2019-11-20 16:28:09 +0700

    Update css

--- resources/views/pages/web/contact.blade.php
+++ resources/views/pages/web/contact.blade.php
@@ -33,7 +33,7 @@
 			</form>
 		</div>
 		<div class="contact-map">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.1062393246775!2d105.7760800144074!3d21.028434693166517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b2f431c099%3A0xe44043bacd461128!2zQuG6v24geGUgTeG7uSDEkMOsbmg!5e0!3m2!1svi!2s!4v1571506147155!5m2!1svi!2s" width="1920" height="657" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.1062393246775!2d105.7760800144074!3d21.028434693166517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b2f431c099%3A0xe44043bacd461128!2zQuG6v24geGUgTeG7uSDEkMOsbmg!5e0!3m2!1svi!2s!4v1571506147155!5m2!1svi!2s" width="2560" height="657" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 		</div>
 	</div>
 @endsection
