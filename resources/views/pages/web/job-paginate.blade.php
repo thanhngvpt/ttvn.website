60feb119d76cb1dac899450edde7e2a6e2cee3b9
GiapHV-TTJ <giaphv@ttjworld.com>
2019-12-03 11:44:22 +0700

    hotfix paginate

--- resources/views/pages/web/job-paginate.blade.php
+++ resources/views/pages/web/job-paginate.blade.php
@@ -4,4 +4,4 @@
         <li class="page-item @if ($data['current_page'] == $i) active @endif child-item" data-page-number="{{$i}}"><a class="page-link">{{$i}}</a></li>
     @endfor
 <li class="page-item next-page"><a class="page-link"><i class="fas fa-chevron-right"></i></a></li>
-endif
+@endif
