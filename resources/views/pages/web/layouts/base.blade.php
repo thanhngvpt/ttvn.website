1477d4c23a06fb8885cfe5f0822ddfab505ad1c1
GiapHV-TTJ <giaphv@ttjworld.com>
2019-12-03 15:20:33 +0700

    update

--- resources/views/pages/web/layouts/base.blade.php
+++ resources/views/pages/web/layouts/base.blade.php
@@ -5,7 +5,7 @@
   	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
 	  <meta name="csrf-token" content="{{ csrf_token() }}">
-	  <link rel="shortcut icon" type="image/png" href="{!! asset('images/logo.png') !!}"/>
+	  <link rel="shortcut icon" type="image/png" href="{!! asset('images/Group82.png') !!}"/>
   	<title>@yield('title')</title>
   	<link rel="shortcut icon" href="/favicon.ico">
 
