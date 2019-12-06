2c8d62ba0829f6b10521d7ae8c8e4cd9e28d68f7
hoanggiap <giapbka57@gmail.com>
2019-12-06 14:44:48 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -53,9 +53,9 @@
     
     // Route::get('gioi-thieu-tap-doan/{type}', 'Web\IntroduceCompanyController@index');
     
+    Route::get('get-project-via-field', 'Web\ScopeActiveController@getProjects');
     Route::get('/{slug}', 'Web\ScopeActiveController@index');
     Route::get('tin-tuc/{category}', 'Web\NewsController@index');
     Route::get('tin-tuc/{category}/{slug}', 'Web\NewsController@details');
-    Route::get('get-project-via-field', 'Web\ScopeActiveController@getProjects');
     Route::post('post-contact', 'Web\ContactController@index')->name('post-contact');
 });
